c1ba583d4dcae1e1064fd5c12905e23b279e5107
ricardo larrahondo <102176871+ricardorlg-yml@users.noreply.github.com>
2022-12-01 20:35:38 +0100

    Update Day01.kt

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,16 +1,33 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun parseInput(input: List<String>): List<Long> {
+        val tot = mutableListOf<Long>()
+        var accum = 0L
+        input.forEach { v ->
+            if (v.isNotBlank()) {
+                accum += v.toLong()
+            } else {
+                tot.add(accum)
+                accum = 0L
+            }
+        }
+        tot.add(accum)//add last one
+        return tot.sorted()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Long {
+        return parseInput(input).last()
+    }
+
+    fun part2(input: List<String>): Long {
+        return parseInput(input)
+            .takeLast(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
+    check(part1(testInput) == 24000L)
+    check(part2(testInput) == 45000L)
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
