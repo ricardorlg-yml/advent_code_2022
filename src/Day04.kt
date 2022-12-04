bf01ae980dfd8031e0895063960f7680b65368c9
ricardo larrahondo <102176871+ricardorlg-yml@users.noreply.github.com>
2022-12-04 12:11:38 +0100

    solve day4

---
+++ src/Day04.kt
@@ -0,0 +1,37 @@
+fun main() {
+    data class Section(val x: Int, val y: Int) {
+        infix fun fullyContains(other: Section): Boolean {
+            return x <= other.x && y >= other.y
+        }
+
+        infix fun overlaps(other: Section): Boolean {
+            return !(other.x > y || other.y < x)
+        }
+    }
+
+    fun String.toPoint(): Section {
+        val (x, y) = split("-").map { it.toInt() }
+        return Section(x, y)
+    }
+
+    fun part1(input: List<String>): Int {
+        return input.count { l ->
+            val (a, b) = l.split(",").map { it.toPoint() }
+            a fullyContains b || b fullyContains a
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.count { l ->
+            val (a, b) = l.split(",").map { it.toPoint() }
+            a overlaps b || b overlaps a
+        }
+    }
+
+    val testInput = readInput("Day04_test")
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
+    val input = readInput("Day04")
+    println(part1(input))
+    print(part2(input))
+}
