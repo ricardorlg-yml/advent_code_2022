0e9157f8fd31b396ab256776ed731a7ccbf1a2fe
ricardo larrahondo <102176871+ricardorlg-yml@users.noreply.github.com>
2022-12-08 08:29:00 +0100

    solve day 8

---
+++ src/Day08.kt
@@ -0,0 +1,96 @@
+import Day08Solver.Direction.*
+
+class Day08Solver(private val input: List<List<Int>>, private val size: Int = input.size) {
+    enum class Direction { LEFT, RIGHT, UP, DOWN }
+
+    private fun generateNeighbours(direction: Direction, row: Int, col: Int): Sequence<Int> {
+        val range = when (direction) {
+            LEFT -> (col - 1 downTo 0)
+            RIGHT -> (col + 1 until size)
+            UP -> (row - 1 downTo 0)
+            DOWN -> (row + 1 until size)
+        }
+        return sequence {
+            for (i in range) {
+                yield(
+                    when (direction) {
+                        LEFT, RIGHT -> input[row][i]
+                        UP, DOWN -> input[i][col]
+                    }
+                )
+            }
+        }
+    }
+
+    fun part1(): Int {
+        var visibleTrees = size * size - ((size - 2) * (size - 2))
+        for (row in 1 until size - 1) {
+            for (col in 1 until size - 1) {
+                val treeHeight = input[row][col]
+                val treesOnTheLeft = generateNeighbours(LEFT, row, col)
+                val treesOnTheRight = generateNeighbours(RIGHT, row, col)
+                val treesFromTop = generateNeighbours(UP, row, col)
+                val tressFromBottom = generateNeighbours(DOWN, row, col)
+                if (treesOnTheLeft.all { treeHeight > it } || treesOnTheRight.all { treeHeight > it } || treesFromTop.all { treeHeight > it } || tressFromBottom.all { treeHeight > it }) {
+                    visibleTrees++
+                    continue
+                }
+            }
+        }
+        return visibleTrees
+    }
+
+    fun part2(): Int {
+        var result = Int.MIN_VALUE
+        for (row in 1 until size - 1) {
+            for (col in 1 until size - 1) {
+                val treeHeight = input[row][col]
+                val treesOnTheLeft = generateNeighbours(LEFT, row, col)
+                val treesOnTheRight = generateNeighbours(RIGHT, row, col)
+                val treesFromTop = generateNeighbours(UP, row, col)
+                val tressFromBottom = generateNeighbours(DOWN, row, col)
+                val visibleLeftTrees = countVisibleTrees(treesOnTheLeft, treeHeight)
+                val visibleRightTress = countVisibleTrees(treesOnTheRight, treeHeight)
+                val visibleTopTrees = countVisibleTrees(treesFromTop, treeHeight)
+                val visibleBottomTrees = countVisibleTrees(tressFromBottom, treeHeight)
+                val scenicScore = visibleLeftTrees * visibleRightTress * visibleTopTrees * visibleBottomTrees
+                if (scenicScore > result) {
+                    result = scenicScore
+                }
+            }
+        }
+        return result
+    }
+
+    private fun countVisibleTrees(neighbors: Sequence<Int>, current: Int): Int {
+        var score = 0
+        for (neighbor in neighbors) {
+            if (neighbor >= current) {
+                score++
+                break
+            } else {
+                score++
+            }
+        }
+        return score
+    }
+}
+
+
+fun main() {
+
+    fun parseInput(input: List<String>): List<List<Int>> {
+        return input.map { line ->
+            line.split("").mapNotNull { it.toIntOrNull() }
+        }
+    }
+
+    val testInput = parseInput(readInput("Day08_test"))
+    val testSolver = Day08Solver(testInput)
+    check(testSolver.part1() == 21)
+    check(testSolver.part2() == 8)
+    val input = readInput("Day08").run { parseInput(this) }
+    val solver = Day08Solver(input)
+    println("Part 1: ${solver.part1()}")
+    println("Part 2: ${solver.part2()}")
+}
